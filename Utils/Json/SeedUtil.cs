bd43c9036685f6ad4d36b91ae205dcead2466a9a
sajad <salimzadehsajad@gmail.com>
2024-01-22 23:26:15 +0330

    move entities to databases

--- Utils/Json/SeedUtil.cs
+++ Utils/Json/SeedUtil.cs
@@ -12,18 +12,17 @@ namespace Dorbit.Framework.Utils.Json;
 
 public static class SeedUtil
 {
-    public static async Task SeedAsync<T>(this IWriterRepository<T> repository, string filename, 
-        Func<T, bool> predicate,
-        Action<T> beforeInsert = default)
+    public static async Task SeedAsync<T>(this IWriterRepository<T> repository, string filename, Action<T> beforeInsert = default)
         where T : class, IEntity
     {
         var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename);
         var content = await File.ReadAllTextAsync(path);
         var items = JsonConvert.DeserializeObject<List<T>>(content);
-        var existsItems = (await repository.Set().ToListAsync()).Where(predicate);
+        var existsItems = await repository.Set().ToListAsync();
         using var transaction = repository.DbContext.BeginTransaction();
         foreach (var entity in items.Where(x => !existsItems.Contains(x)))
         {
+            if (beforeInsert is not null) beforeInsert(entity);
             await repository.InsertAsync(entity);
         }
 
