5e0505f133a6db52e2e448763fb5d30068f3047b
sajad salimzadeh <salimzadehsajad@gmail.com>
2024-09-23 14:15:02 +0330

    improve: add HttpHelperRequest Dto

---
+++ Utils/Http/HttpHelperRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Dorbit.Framework.Utils.Http;
+
+public class HttpHelperRequest(HttpMethod method, string url)
+{
+    public HttpMethod Method { get; set; } = method;
+    public string Url { get; } = url;
+    public object Parameter { get; set; }
+    public Dictionary<string, string> Headers { get; set; }
+
+    public HttpHelperRequest(HttpMethod method, string url, object parameter) : this(method, url)
+    {
+        Parameter = parameter;
+    }
+
+    public HttpHelperRequest(HttpMethod method, string url, object parameter, Dictionary<string, string> headers) : this(method, url)
+    {
+        Parameter = parameter;
+        Headers = headers;
+    }
+}
