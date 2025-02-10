388ff36a04900eaafd2358756e69809d797a5952
sajad <salimzadehsajad@gmail.com>
2025-02-10 19:20:11 +0330

    bug: fix is not active user login

--- Contracts/Abstractions/IUserDto.cs
+++ Contracts/Abstractions/IUserDto.cs
@@ -6,5 +6,6 @@ public interface IUserDto
 {
     public object Id { get; set; }
     public string Username { get; set; }
+    public bool IsActive { get; set; }
     public ClaimsPrincipal Claims { get; set; }
 }
