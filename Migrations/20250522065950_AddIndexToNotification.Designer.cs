fc8351d4929904686b570d8f331185bc0bf5db50
sajad salimzadeh <salimzadehsajad@gmail.com>
2025-05-22 12:52:36 +0330

    improve: add primeng

--- Migrations/20250522065950_AddIndexToNotification.Designer.cs
+++ Migrations/20250522065950_AddIndexToNotification.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Dorbit.Framework.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Dorbit.Framework.Migrations
 {
     [DbContext(typeof(FrameworkDbContext))]
-    partial class FrameworkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250522065950_AddIndexToNotification")]
+    partial class AddIndexToNotification
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
