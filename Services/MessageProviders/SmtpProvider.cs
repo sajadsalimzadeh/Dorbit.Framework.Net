da8a1568bc0bc6d589ccd640b4c9b6b04e4a93b7
sajad <salimzadehsajad@gmail.com>
2025-08-27 01:24:35 +0330

    improve: email provider

--- Services/MessageProviders/SmtpProvider.cs
+++ Services/MessageProviders/SmtpProvider.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Dorbit.Framework.Attributes;
 using Dorbit.Framework.Contracts.Messages;
 using Dorbit.Framework.Contracts.Results;
 using Dorbit.Framework.Extensions;
 using Dorbit.Framework.Services.Abstractions;
+using MailKit;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using MimeKit;
@@ -17,6 +19,7 @@ public class SmtpProvider : IMessageProvider<MessageEmailRequest, ConfigMessageE
 
     private string _server;
     private short _port;
+    private string _senderName;
     private string _senderEmail;
     private string _username;
     private string _password;
@@ -25,6 +28,7 @@ public class SmtpProvider : IMessageProvider<MessageEmailRequest, ConfigMessageE
     {
         _server = configuration.Server;
         _port = configuration.Port;
+        _senderName = configuration.SenderName;
         _senderEmail = configuration.Sender;
         _username = configuration.Username;
         _password = configuration.Password.GetDecryptedValue();
@@ -32,10 +36,10 @@ public class SmtpProvider : IMessageProvider<MessageEmailRequest, ConfigMessageE
 
     public async Task<QueryResult<string>> SendAsync(MessageEmailRequest request)
     {
-        var email = new MimeMessage();
-        email.Sender = MailboxAddress.Parse(_senderEmail);
-        email.To.Add(MailboxAddress.Parse(request.To));
-        email.Subject = request.Subject;
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_senderName, _senderEmail));
+        message.To.Add(MailboxAddress.Parse(request.To));
+        message.Subject = request.Subject;
         var builder = new BodyBuilder();
         if (request.Attachments != null)
         {
@@ -50,12 +54,12 @@ public class SmtpProvider : IMessageProvider<MessageEmailRequest, ConfigMessageE
             }
         }
 
-        builder.HtmlBody = string.Format(request.Body, request.Args);
-        email.Body = builder.ToMessageBody();
-        using var smtp = new SmtpClient();
-        await smtp.ConnectAsync(_server, _port, SecureSocketOptions.StartTls);
+        message.Body = new TextPart("plain") { Text = string.Format(request.Body, request.Args ?? []) };
+        using var logger = new ProtocolLogger(Console.OpenStandardError());
+        using var smtp = new SmtpClient(logger);
+        await smtp.ConnectAsync(_server, _port);
         await smtp.AuthenticateAsync(_username, _password);
-        await smtp.SendAsync(email);
+        await smtp.SendAsync(message);
         await smtp.DisconnectAsync(true);
 
         return new QueryResult<string>() { Success = true, Data = "" };
