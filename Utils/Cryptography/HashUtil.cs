e1161fea805edb48a938f56432ea89da817ba2b1
sajad salimzadeh <salimzadehsajad@gmail.com>
2025-08-27 18:40:29 +0330

    improve: add md5 hash

--- Utils/Cryptography/HashUtil.cs
+++ Utils/Cryptography/HashUtil.cs
@@ -46,4 +46,20 @@ public static class HashUtil
     {
         return Sha256(secretKey + secretKey + password);
     }
+    
+    public static string Md5(string input, string salt)
+    {
+        byte[] data;
+        using (var md5Hash = MD5.Create())
+        {
+            data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input + salt));
+        }
+        var sBuilder = new StringBuilder();
+
+        foreach (var t in data)
+        {
+            sBuilder.Append(t.ToString("x2"));
+        }
+        return sBuilder.ToString();
+    }
 }
