b08c4ad7a21844854efcd4abc102f80084066617
sajad salimzadeh <salimzadehsajad@gmail.com>
2025-12-09 09:13:37 +0330

    improve: identity request model

--- Contracts/Identities/IdentityRequest.cs
+++ Contracts/Identities/IdentityRequest.cs
@@ -1,13 +1,13 @@
 ﻿namespace Dorbit.Framework.Contracts.Identities;
 
-public class IdentityValidateRequest
+public class IdentityRequest
 {
     public string AccessToken { get; set; }
     public string CsrfToken { get; set; }
     public string UserAgent { get; set; }
+    public string Timezone { get; set; }
     
     public string IpV4 { get; set; }
     public string IpV6 { get; set; }
-    public string Timezone { get; set; }
-    public string PointerToken { get; set; }
+    public LatLng Location { get; set; }
 }
