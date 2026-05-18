62ec7fce9b1d404f84a8450322986f40ff411b1b
sajad <salimzadehsajad@gmail.com>
2026-05-18 07:46:19 +0330

    improve: add round extension for number by coefficient

--- Extensions/NumberExtensions.cs
+++ Extensions/NumberExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Dorbit.Framework.Extensions;
+﻿using System;
+
+namespace Dorbit.Framework.Extensions;
 
 public static class NumberExtensions
 {
@@ -12,5 +14,15 @@ public static class NumberExtensions
     {
         var hex = number.ToString("X");
         return hex.Length % 2 == 0 ? hex : "0" + hex;
+    }
+    
+    public static int Round(this int number, double coefficient)
+    {
+        return (int)(Math.Round(number / coefficient) * coefficient);
+    }
+    
+    public static long Round(this long number, double coefficient)
+    {
+        return (long)(Math.Round(number / coefficient) * coefficient);
     }
 }
