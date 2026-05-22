1c8b8cb7702f36785fb68becdd7658a98b81586d
sajad <salimzadehsajad@gmail.com>
2026-05-22 22:33:13 +0330

    improve: remove empty string http get of crud controller

--- Controllers/CrudController.cs
+++ Controllers/CrudController.cs
@@ -24,7 +24,7 @@ public abstract class CrudController<TEntity, TKey, TGet, TAdd> : CrudController
     
     protected virtual IQueryable<TEntity> Set() => Repository.Set();
 
-    [HttpGet(""), Auth("{type0}-View")]
+    [HttpGet, Auth("{type0}-View")]
     public virtual async Task<QueryResult<IEnumerable<TGet>>> GetAllAsync()
     {
         var query = Set();
