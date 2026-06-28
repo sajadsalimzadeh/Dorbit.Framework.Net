df0b6b00d75a004914bf053c4268d9e35867dd2d
sajad <salimzadehsajad@gmail.com>
2026-06-28 22:06:08 +0330

    improve: get claims identity service

--- Services/Abstractions/IIdentityService.cs
+++ Services/Abstractions/IIdentityService.cs
@@ -1,11 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
 using Dorbit.Framework.Contracts.Identities;
-using Microsoft.AspNetCore.Http;
 
 namespace Dorbit.Framework.Services.Abstractions;
 
 public interface IIdentityService
 {
     IdentityDto Identity { get; }
+    ClaimsPrincipal GetClaimsPrincipal(string accessToken);
     Task<IdentityDto> ValidateAsync(IdentityRequest request);
 }
