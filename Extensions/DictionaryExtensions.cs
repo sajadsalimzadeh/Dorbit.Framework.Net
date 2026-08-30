57fc1405bb862142fff7d4267c71495be62024dc
sajad salimzadeh <salimzadehsajad@gmail.com>
2026-08-30 23:50:57 +0330

    improve: add Dictionary GetInt32 Double , ....

--- Extensions/DictionaryExtensions.cs
+++ Extensions/DictionaryExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Dorbit.Framework.Extensions;
 
@@ -9,4 +11,23 @@ public static class DictionaryExtensions
         if (dict != null && dict.TryGetValue(key, out var value)) return value;
         return default;
     }
+    
+    public static int GetInt32OrDefault<TKey>(this Dictionary<TKey, JsonElement> dict, TKey key)
+    {
+        if (dict != null && dict.TryGetValue(key, out var value)) return value.GetInt32();
+        return 0;
+    }
+    
+    public static double GetDoubleOrDefault<TKey>(this Dictionary<TKey, JsonElement> dict, TKey key)
+    {
+        if (dict != null && dict.TryGetValue(key, out var value)) return value.GetDouble();
+        return 0;
+    }
+    
+    public static string GetStringOrDefault<TKey>(this Dictionary<TKey, JsonElement> dict, TKey key)
+    {
+        if (dict != null && dict.TryGetValue(key, out var value)) return value.GetString();
+        return null;
+    }
+    
 }
